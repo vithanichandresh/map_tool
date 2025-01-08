a204db55da52e3ef43375606019de41c03f0cb45
Chandresh <vithanichandresh.g@gmail.com>
2025-01-08 14:41:02 +0530

    initial code commit

--- lib/project_selection.dart
+++ lib/project_selection.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
@@ -13,10 +15,27 @@ class ProjectSelectionState extends State<ProjectSelection> {
 
   Future<void> _pickDirectory() async {
     String? directoryPath = await FilePicker.platform.getDirectoryPath();
+
+    // check directory have pubspec.yaml file or not
+    bool isPubspecFileExist = await File('$directoryPath/pubspec.yaml').exists();
+    if (!isPubspecFileExist && mounted) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Selected directory does not contain pubspec.yaml file.')));
+    }
     if (directoryPath != null) {
-      setState(() {
-        _selectedDirectory = directoryPath;
-      });
+      _selectedDirectory = directoryPath;
+      File file = File('$directoryPath/pubspec.yaml');
+      String contents = await file.readAsString();
+      print(contents);
+
+      var lines = contents.split('\n');
+      var dependenciesIndex = lines.indexWhere((element) => element.contains('dependencies:'));
+      if (dependenciesIndex != -1) {
+        lines.insert(dependenciesIndex + 1, '  google_maps_flutter: ^2.10.0');
+      }
+      contents = lines.join('\n');
+      await file.writeAsString(contents);
+      print(contents);
+      setState(() {});
     }
   }
 
